03645ed5715881c9f6ae2af765ed4f14bc0cbce2
William Siqueira <william.fatecsjc@gmail.com>
2018-10-19 00:52:48 -0300

    Adding a CRUD example

---
+++ crud-produto/src/main/java/org/fatecsjc/crud/crudproduto/ProdutoApp.java
@@ -0,0 +1,9 @@
+package org.fatecsjc.crud.crudproduto;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class ProdutoApp extends Application {
+
+}
